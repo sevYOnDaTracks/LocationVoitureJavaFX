083de179e0fbc5d1f4f3d7b5f160900f4a08a2d6
sevYOnDaTracks <118760727+sevYOnDaTracks@users.noreply.github.com>
2023-03-05 15:56:27 +0100

    finish first step

--- src/main/java/fr/location/abiproject/MainController.java
+++ src/main/java/fr/location/abiproject/MainController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -17,6 +18,8 @@ public class MainController {
     @FXML
     void SeDeconnecter(ActionEvent event) {
         // Rediriger vers la page d'inscription
+
+        showAlert("Vous allez être déconnecté");
         Stage stage = (Stage) seDeconnecter.getScene().getWindow();
         // Charger le fichier FXML de la page d'inscription
         FXMLLoader loader = new FXMLLoader(getClass().getResource("acceuil.fxml"));
@@ -26,5 +29,12 @@ public class MainController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
